cf0aa640904acb80ab3c1b9fcb19bbfd061cf819
Kyrylo <shandaliuk3019@gmail.com>
2023-01-19 15:32:02 +0200

    Added initial files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import { SharedLayout } from './SharedLayout';
+import { Home } from 'pages/Home';
+import { Movies } from 'pages/Movies';
+import { Movie } from 'pages/Movie';
+import { Cast } from './Cast';
+import { Reviews } from './Reviews';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<Movie />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 };
